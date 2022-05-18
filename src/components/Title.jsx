940cd65129545f7abc3ae430aa57ba0b7edb74f0
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-05-18 00:25:04 -0700

    created title page with animation

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import neon from './title.module.css'
+
+export default function Title() {
+
+    return (
+        <body>
+        <container className={neon.title}>
+            <h1 className={neon.neonTitle}>Drunkn'Cookn</h1>
+        </container>
+        </body>
+    )
+}
